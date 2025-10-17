72618140c501ffd20c89f3d72cf56e24ec259eba
mar <gim.marcello@gmail.com>
2025-10-17 08:29:50 +0200

    changed: ignoring some confs and writer in main

--- ay38910a.nim
+++ ay38910a.nim
@@ -1,17 +1,20 @@
 import avr_io
 import delay
+
+import writer
 import psg
 
 
 proc genClock(clockPort: Port, clockPin: uint8) =
-  clockPort.asOutputPin(clockPin)
-  OCR2A[] = 3
   timer2.setTimerFlag({TimCtlAFlag.wgm1, coma0})
   timer2.setTimerFlag({TimCtlBFlag.cs0})
+  clockPort.asOutputPin(clockPin)
+  OCR2A[] = 3
+
 
 proc loop =
   genClock(portB, 4)
-  const ay = ay38910a(bc1Pin: 4, bdirPin: 5, ctlPort: portH, dataPort: portA)
+  const ay = ay38910a[Port](bc1: 4, bdir: 5, ctl: portH, dataPort: portA)
   ay.init()
   ay.channelOn({CHA_TONE})
   ay.setAmplitude(CHAN_A, 15, false)
